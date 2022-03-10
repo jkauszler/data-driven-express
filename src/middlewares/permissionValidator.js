6d5112467ef32dae35459c5e25eb4c4bfc4e77d3
Justin Kauszler <jkauszler@gmail.com>
2022-03-10 14:17:49 -0500

    fix: removing console.log

--- src/middlewares/permissionValidator.js
+++ src/middlewares/permissionValidator.js
@@ -3,8 +3,6 @@ const hasPermission = (arr1, arr2) => arr1.some((el) => arr2.includes(el));
 const permissionValidator = (allowedPermissions) => (req, res, next) => {
 	const permissions = req?.user?.permissions || [];
 
-	console.log(allowedPermissions.length === 0);
-
 	// bail out if no permissions passed
 	if (allowedPermissions.length === 0) { next(); return; }
 
